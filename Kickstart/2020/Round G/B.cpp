5f5d89e09da2abccc843bf4efbcce17fcd356135
Sashank Shukla <ayaz.shukla@gmail.com>
2022-04-12 23:23:29 -0700

    update

--- Kickstart/2020/Round G/B.cpp
+++ Kickstart/2020/Round G/B.cpp
@@ -6,28 +6,23 @@ using namespace std; typedef  long  long  ll; typedef vector<int> vi; typedef se
 #define print(i,s) cout << "Case #" << i+1 << ": " << s << "\n";
 int main(){
 ios::sync_with_stdio(0);cin.tie(0); int t; cin >> t;
-FOR(t){
-	int n; cin >> n; int arr[n][n];
+for(int i=0;i<t;++i){
+	int n; cin >> n;
+	int arr[n][n];
 	for(int r=0;r<n;++r){
-        for(int c=0;c<n;++c){
-            cin >> arr[r][c];
-        }
-    }
-    int reps=(n*2)-1; ll m=(ll)0;
-	for(int j=0;j<reps;++j){
-	    int x=0,y=0; ll sum=(ll)0;
-        if(j<n){
-		 x=n-j-1,y=0;
-		}
-        else{
-         x=0,y=j-n+1;
-        }
-		while(x<n && y<n){
-			sum+=(ll)arr[x][y];
-			++x; ++y;
-		}
-		m=max(sum,m);
+       for(int c=0;c<n;++c){
+		   cin >> arr[r][c];
+	   }
 	}
-	  print(i,m);
-  }
+	map<int, ll> mp;
+	ll res = (ll)0;
+	for(int r=0;r<n;++r){
+		for(int c=0;c<n;++c){
+			mp[r-c]+= (ll)arr[r][c];
+			if(mp[r-c] > res)
+			  res = mp[r-c];
+		}
+	}
+	print(i,res);
+ }
 }

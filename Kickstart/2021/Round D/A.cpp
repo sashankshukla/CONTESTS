2428b88cdb5f894e94408073d80ecb97a98e3348
Sashank Shukla <ayaz.shukla@gmail.com>
2022-04-12 22:18:21 -0700

    update

---
+++ Kickstart/2021/Round D/A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int dRow[] = { -1, 0, 1, 0 }, dCol[] = { 0, 1, 0, -1 };//direction vectors
+const int mxn=1e5; vector<int> adj[mxn]; bool visited[mxn]; int d[mxn];
+typedef long  long  ll;
+#define MOD 1000000007
+#define pb  push_back
+#define FOR(a)     for(int i=0;i<a;++i)
+#define sort(v)    sort(v.begin(),v.end());
+#define reverse(v) reverse(v.begin(),v.end());
+#define print(i,s) cout << "Case #" << i+1 << ": " << s << "\n";
+#define line()     "\n"
+
+int main(){
+ios::sync_with_stdio(0);cin.tie(0);
+int t; cin >> t;
+FOR(t){
+    int a[3][3];
+    for(int i=0;i<3;++i){
+        for(int j=0;j<3;++j){
+            if(i!=1 && j!=1)
+              cin >> a[i][j];
+        }
+    }
+ }
+}

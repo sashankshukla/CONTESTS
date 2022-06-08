deae943c38370d190c21bdc7f44b9f70d01cc08e
Sashank Shukla <ayaz.shukla@gmail.com>
2022-06-07 19:31:37 -0700

    update

--- Kickstart/2020/Round G/B.cpp
+++ Kickstart/2020/Round G/B.cpp
@@ -1,28 +1,33 @@
 #include <bits/stdc++.h>
-using namespace std; typedef  long  long  ll; typedef vector<int> vi; typedef set<int> si;
-#define FOR(a)  for(int i=0;i<a;++i)
-#define add(a) v.push_back(a);
-#define sort(v) sort(v.begin(),v.end());
+using namespace std;
+int dRow[] = { -1, 0, 1, 0 }, dCol[] = { 0, 1, 0, -1 };//direction vectors
+const int mxn=1e5; vector<int> adj[mxn]; bool visited[mxn]; int d[mxn];
+typedef long  long  ll;
+#define MOD 1000000007
+#define pb  push_back
+#define FOR(a)     for(int i=0;i<a;++i)
+#define sort(v)    sort(v.begin(),v.end());
+#define reverse(v) reverse(v.begin(),v.end());
 #define print(i,s) cout << "Case #" << i+1 << ": " << s << "\n";
+#define line()   "\n"
+
 int main(){
-ios::sync_with_stdio(0);cin.tie(0); int t; cin >> t;
-for(int i=0;i<t;++i){
-	int n; cin >> n;
-	int arr[n][n];
-	for(int r=0;r<n;++r){
-       for(int c=0;c<n;++c){
-		   cin >> arr[r][c];
-	   }
-	}
-	map<int, ll> mp;
-	ll res = (ll)0;
-	for(int r=0;r<n;++r){
-		for(int c=0;c<n;++c){
-			mp[r-c]+= (ll)arr[r][c];
-			if(mp[r-c] > res)
-			  res = mp[r-c];
-		}
-	}
-	print(i,res);
- }
+  ios::sync_with_stdio(0);cin.tie(0);
+  int t; cin >> t;
+  for(int i=0;i<t;++i){
+    int n; cin >> n;
+    map<int,ll> mp;
+    for(int x=0;x<n;++x){
+      for(int y=0;y<n;++y){
+        int val; cin >> val;
+        mp[x-y]+=val;
+      }
+    }
+    ll res = -1;
+    for(pair<int,ll> p: mp){
+      if(p.second > res)
+        res = p.second;
+    }
+    print(i,res);
+  }
 }

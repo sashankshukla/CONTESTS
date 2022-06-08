58f35563bc7a85f7488987e1b485e2574a7fca74
Sashank Shukla <ayaz.shukla@gmail.com>
2022-06-07 19:35:34 -0700

    update

--- Runner/template.cpp
+++ Runner/template.cpp
@@ -13,33 +13,4 @@ typedef long  long  ll;
 
 int main(){
   ios::sync_with_stdio(0);cin.tie(0);
-  int t; cin >> t;
-  for(int i=0;i<t;++i){
-    int w,n; cin >> w >> n;
-    vector<int> v;
-    for(int j=0;j<w;++j){
-      int val; cin >> val;
-      v.push_back(val);
-    }
-    sort(v);
-    ll ans = (ll)1e18;
-    for(int j=0;j<w;++j){
-       int best = v[j];
-       ll res = 0;
-       for(int val : v){
-       int a,b; 
-       if(val <= best){
-         a = best-val;
-         b = n-best+val;
-        }
-        else{
-         a = val-best;
-         b = n+best-val;
-        }
-       res+=(ll)min(a,b);
-     }
-     ans = min(ans,res);
-    }
-    print(i,ans);
-  }
 }

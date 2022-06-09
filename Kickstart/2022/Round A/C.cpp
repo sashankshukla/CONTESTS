3b3d2293b7e642616e99d4c41eca5475cfe124de
Sashank Shukla <ayaz.shukla@gmail.com>
2022-06-08 21:30:58 -0700

    update

--- Kickstart/2022/Round A/C.cpp
+++ Kickstart/2022/Round A/C.cpp
@@ -9,13 +9,87 @@ typedef long  long  ll;
 #define sort(v)    sort(v.begin(),v.end());
 #define reverse(v) reverse(v.begin(),v.end());
 #define print(i,s) cout << "Case #" << i+1 << ": " << s << "\n";
-#define line()     "\n"
+#define line()   "\n"
+
+  
+
+vector<string> perms; // all possible strings after changing question mark
+// void permute(string a,int l, int r){
+
+//   if(a.find('?') == string::npos && find(perms.begin(),perms.end(),a) == perms.end()){
+//        perms.push_back(a);
+//        return;
+//   }
+//   for(int i=l;i<=r;++i){
+//       if(a[i] == '?'){
+//         a[i]='0';
+//         permute(a,l+1,r);
+//         a[i]='1';
+//         permute(a,l+1,r);
+//       }
+//    }
+// }
+
+void permute(string a, int index){
+   bool chk = false;
+   int temp = index;
+   for(int i=index;i<a.size();++i){
+      if(a[i] == '?'){
+        chk = true;
+        temp = i;
+        break;
+      }
+   }
+   if(!chk){
+     perms.push_back(a);
+     return;
+   }
+   permute(a.replace(temp,1,"0"),temp+1);
+   permute(a.replace(temp,1,"1"),temp+1);
+}
+
+
+bool isPalindrome(string s){
+  string rev(s.rbegin(),s.rend());
+  return rev == s;
+}
 
 int main(){
-ios::sync_with_stdio(0);cin.tie(0);
-int t; cin >> t;
-FOR(t){
-   int n; cin >> n;
-   string s; cin >> s;
- }
-}
+  ios::sync_with_stdio(0);cin.tie(0);
+  int t; cin >> t;
+  for(int i=0;i<t;++i){
+    perms.clear();
+    int n; cin >> n;
+    string s; cin >> s;
+    permute(s,0);
+    bool res = false;
+    for(string p : perms){
+      if(p.size() < 5){
+        res = true;
+        break;
+      }
+      else{
+        bool chk = true;
+        for(int j=0;j<p.size();++j){
+          for(int k=5;k+j-1<p.size() && k<=6;++k){
+             string sub = p.substr(j,k);
+             if(isPalindrome(sub)){
+               chk = false;
+               j = p.size();
+               break;
+             }
+          }
+        }
+        if(chk){
+        res = true;
+        break;
+        }
+      }
+    }
+    if(res){
+      print(i,"POSSIBLE");}
+    else{
+      print(i,"IMPOSSIBLE");
+    }
+  }
+} 
--- Runner/template.cpp
+++ Runner/template.cpp
@@ -14,60 +14,80 @@ typedef long  long  ll;
   
 
 vector<string> perms; // all possible strings after changing question mark
-void permute(string a,int l, int r){
-  if(a.find('?') == string::npos && find(perms.begin(),perms.end(),a) == perms.end()){
-       perms.push_back(a);
-       return;
-  }
-  for(int i=l;i<=r;++i){
+// void permute(string a,int l, int r){
+
+//   if(a.find('?') == string::npos && find(perms.begin(),perms.end(),a) == perms.end()){
+//        perms.push_back(a);
+//        return;
+//   }
+//   for(int i=l;i<=r;++i){
+//       if(a[i] == '?'){
+//         a[i]='0';
+//         permute(a,l+1,r);
+//         a[i]='1';
+//         permute(a,l+1,r);
+//       }
+//    }
+// }
+
+void permute(string a, int index){
+   bool chk = false;
+   int temp = index;
+   for(int i=index;i<a.size();++i){
       if(a[i] == '?'){
-        a[i]='0';
-        permute(a,l+1,r);
-        a[i]='1';
-        permute(a,l+1,r);
+        chk = true;
+        temp = i;
+        break;
       }
    }
+   if(!chk){
+     perms.push_back(a);
+     return;
+   }
+   permute(a.replace(temp,1,"0"),temp+1);
+   permute(a.replace(temp,1,"1"),temp+1);
 }
+
 
 bool isPalindrome(string s){
   string rev(s.rbegin(),s.rend());
-  return rev==s;
+  return rev == s;
 }
 
 int main(){
   ios::sync_with_stdio(0);cin.tie(0);
   int t; cin >> t;
   for(int i=0;i<t;++i){
-    int n; cin >> n; string s; cin >> s;
-    permute(s,0,n-1);
+    perms.clear();
+    int n; cin >> n;
+    string s; cin >> s;
+    permute(s,0);
     bool res = false;
-    for(string perm : perms){
-      cout << perm << endl;
-      if(perm.size() < 5){
+    for(string p : perms){
+      if(p.size() < 5){
         res = true;
         break;
       }
       else{
         bool chk = true;
-        for(int j=0;j<perm.size();++j){
-          for(int k=5;k+j-1<perm.size() && k<=6;++k){
-             string sub = perm.substr(j,k);
+        for(int j=0;j<p.size();++j){
+          for(int k=5;k+j-1<p.size() && k<=6;++k){
+             string sub = p.substr(j,k);
              if(isPalindrome(sub)){
                chk = false;
-               j = perm.size();
+               j = p.size();
                break;
              }
           }
         }
         if(chk){
-          res = true;
-          break;
+        res = true;
+        break;
         }
-      } 
+      }
     }
     if(res){
-      print(i,"POSSIBLE");
-    }
+      print(i,"POSSIBLE");}
     else{
       print(i,"IMPOSSIBLE");
     }

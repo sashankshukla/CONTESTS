8c24c71b6ef0aded25a0af95828b15bca2fd422c
Sashank Shukla <ayaz.shukla@gmail.com>
2022-07-15 23:52:59 -0700

    update

--- Hacker Cup/2021/Round 1/A2.cpp
+++ Hacker Cup/2021/Round 1/A2.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int dRow[] = { -1, 0, 1, 0 }, dCol[] = { 0, 1, 0, -1 };//direction vectors
 typedef long  long  ll;
-#define MOD 1000000007
+const ll MOD = 1000000007;
 #define PI acos(-1)
 #define FOR(a)     for(int it=0;it<a;++it)
 #define print(i,s) cout << "Case #" << i+1 << ": " << s << "\n";
@@ -11,8 +11,48 @@ typedef long  long  ll;
 ofstream myfile;
 #define f_init()  myfile.open("output.txt"); cout << "f_init" << line();
 #define f_close() myfile.close(); cout << "f_close" << line();
-                  
+
+
+ll calc(string s){
+    ll total = 0;
+    ll res = 0;
+    bool picked = false;
+    bool hand; // false for left, true for right
+    string temp = "";
+    for(char ch : s){
+        temp += ch ;
+        if(ch == 'X' && !picked){
+            picked = true;
+            hand = false;
+        }
+        else if(ch == 'O' && !picked){
+            picked = true;
+            hand = true;
+        }
+        else if(ch == 'O' && !hand){
+            ++res;
+            hand = true;
+        }
+        else if(ch == 'X' && hand){
+            ++res;
+            hand = false;
+        }
+        total += res;
+    }
+    return total;
+}     
  
 int main(){
-  ios::sync_with_stdio(0);cin.tie(0);
+  ios::sync_with_stdio(0);cin.tie(0); f_init();
+  int t; cin >> t;
+  FOR(t){
+    int n; cin >> n;
+    ll res = 0;
+    string s; cin >> s;
+    for(int i=0;i<s.size();++i){
+        res+= calc(s.substr(i,n-i))%MOD;  
+    }
+    write(it,res);
+  }
+  f_close();
 }

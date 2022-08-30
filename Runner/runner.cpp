0cfa2e899405e3a7635e1480d0fa0f8417976086
Sashank Shukla <ayaz.shukla@gmail.com>
2022-08-30 11:50:26 -0700

    update

--- Runner/runner.cpp
+++ Runner/runner.cpp
@@ -8,27 +8,42 @@ const ll  MOD = 1000000007;
 #define print(i,s) cout << "Case #" << i+1 << ": " << s << "\n";
 #define line() "\n" 
 
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
 template <typename T>
 T output_vector(vector<T> v) { for(auto val : v) cout << val << " "; cout << line();}
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
 
-void dbg_out() { cerr << endl;}
-template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) {cerr << " " << H; dbg_out(T...);} 
-#ifdef SASHANK_DEBUG
-#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
-else
-#define dbg(...)
+#ifndef DEBUG
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
 #endif
- 
 
 void solve(){
-  
+   
 }
 
 int main(){
- ios::sync_with_stdio(0);
- #ifndef SASHANK_DEBUG 
- cin.tie(nullptr); 
- #endif
+ ios::sync_with_stdio(0); cin.tie(0);
  int T; cin >> T;
  FOR(T){
   cout << "Case #" << t+1 << ": ";

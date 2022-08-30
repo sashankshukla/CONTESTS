6a904faa20972b3ebefa010cf3ec63f9009e1681
Sashank Shukla <ayaz.shukla@gmail.com>
2022-08-30 14:16:36 -0700

    updated

--- Kickstart/2022/Round E/B.cpp
+++ Kickstart/2022/Round E/B.cpp
@@ -1,44 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
-int dRow[] = { -1, 0, 1, 0 }, dCol[] = { 0, 1, 0, -1 };//direction vectors
+int dRow[] = { -1, 0, 1, 0 }, dCol[] = { 0, 1, 0, -1 };//direction vectors 
 typedef long  long  ll;
 const ll  MOD = 1000000007;
 #define PI acos(-1)
-#define FOR(a)     for(int it=0;it<a;++it)
+#define FOR(a)     for(int t=0;t<a;++t)
 #define print(i,s) cout << "Case #" << i+1 << ": " << s << "\n";
 #define line() "\n" 
-                  
- 
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template <typename T_vector>
+void output_vector(const T_vector &v) { for(auto val : v) cout << val << " "; cout << line();}
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+
+#ifndef DEBUG
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+void solve(){
+   int n; cin >> n;
+   vector<int> v(n);
+   for(auto &val : v) cin >> val;
+   vector<int> s = v; sort(s.begin(),s.end());
+   vector<int> mentor(n);
+   for(int i=0;i<n;++i){
+     int target = 2*v[i];
+     auto upper = upper_bound(s.begin(),s.end(),target);
+     --upper;
+     if(s[upper - s.begin()] == v[i])
+        --upper;
+     if(upper - s.begin() == -1)  mentor[i] = -1;
+     else mentor[i] = s[upper-s.begin()];
+   }
+   output_vector(mentor);
+}
+
 int main(){
-  ios::sync_with_stdio(0);cin.tie(0);
-  int t; cin >> t;
-  FOR(t){
-    int n; cin >> n;
-    vector<int> s(n);
-    for(int i=0;i<n;++i){
-      cin >> s[i];
-    }
-    vector<int> temp = s;
-    cout << "Case #" << it+1 << ": ";
-    sort(temp.begin(),temp.end());
-    for(int i=0;i<s.size();++i){
-        int target = 2*s[i];
-        int res = -1;
-        int l = 0,r=temp.size()-1;
-        while(l<=r){
-            int m = l+(r-l)/2;
-            if(temp[m] <= target){
-                if(temp[m] == s[i] && m>0)
-                   res = max(temp[m-1],res);
-                else if(temp[m] != s[i])
-                   res = temp[m];
-                l = m+1;
-            }
-            else
-               r = m-1;
-        }
-        cout << res << " ";
-    }
-    cout << line();
-  }
+ ios::sync_with_stdio(0); cin.tie(0);
+ int T; cin >> T;
+ FOR(T){
+  cout << "Case #" << t+1 << ": ";
+  solve();
+ }
 }
--- Kickstart/2022/Round E/C.cpp
+++ Kickstart/2022/Round E/C.cpp
@@ -1,47 +1,85 @@
 #include <bits/stdc++.h>
 using namespace std;
-int dRow[] = { -1, 0, 1, 0 }, dCol[] = { 0, 1, 0, -1 };//direction vectors
+int dRow[] = { -1, 0, 1, 0 }, dCol[] = { 0, 1, 0, -1 };//direction vectors 
 typedef long  long  ll;
 const ll  MOD = 1000000007;
 #define PI acos(-1)
-#define FOR(a)     for(int it=0;it<a;++it)
+#define FOR(a)     for(int t=0;t<a;++t)
 #define print(i,s) cout << "Case #" << i+1 << ": " << s << "\n";
 #define line() "\n" 
 
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template <typename T_vector>
+void output_vector(const T_vector &v) { for(auto val : v) cout << val << " "; cout << line();}
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.f); cerr << ','; __print(x.s); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+
+#ifndef DEBUG
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+
+
+string res = "";
+int minLength = 1e9+1;
+
 bool isPalindrome(string s){
-    string rev(s.rbegin(),s.rend());
-    return s == rev;
-}             
- 
+   string rev(s.rbegin(),s.rend());
+   return s == rev;
+}
+
+void factorize(int len ,string s){
+    for(int i=1;i*i <= len;++i){
+        if(len%i == 0){
+            int other = len/i;
+            string sub = s.substr(0,i);
+            if(isPalindrome(sub) && isPalindrome(s+sub) && sub.size() < minLength){
+              res = sub; minLength = sub.size();
+            }
+            if(i != other){
+                sub = s.substr(0,other);
+                if(isPalindrome(sub) && isPalindrome(s+sub) && sub.size() < minLength){
+                   res = sub; minLength = sub.size();
+            }
+          }
+        }
+    }
+}
+
+void solve(){
+  int n; cin >> n;
+  string s; cin >> s;
+   int len = (int)s.size();
+   factorize(len,s);
+   cout << res << line();
+   minLength = 1e9+1;
+}
+
 int main(){
-  ios::sync_with_stdio(0);cin.tie(0);
-  int t; cin >> t;
-  FOR(t){
-    int n; cin >> n;
-    string s; cin >> s;
-    vector<string> p;
-    for(int i=0;i<s.size();++i){
-        int l=i,r=i;
-        while(l>=0 && r<s.size() && s[l] == s[r]){
-            p.push_back(s.substr(l,r-l+1));
-            --l;
-            ++r;
-        }
-    }
-    for(int i=0;i<s.size()-1;++i){
-        int l=i,r=i+1;
-        while(l>=0 && r<s.size() && s[l] == s[r]){
-            p.push_back(s.substr(l,r-l+1));
-            --l;
-            ++r;
-        }
-    }
-    int minLength = 1e9+1;
-    string res;
-    for(string pal : p) if(isPalindrome(pal+s) && pal.size() < minLength){
-        minLength = pal.size();
-        res = pal;
-    }
-    print(it,res);
-  }
+ ios::sync_with_stdio(0); cin.tie(0);
+ int T; cin >> T;
+ FOR(T){
+  cout << "Case #" << t+1 << ": ";
+  solve();
+ }
 }

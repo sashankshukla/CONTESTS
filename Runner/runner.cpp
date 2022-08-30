9cff64e33726e8e02c7efe39c7c28bfe6ba2d0d2
Sashank Shukla <ayaz.shukla@gmail.com>
2022-08-30 14:37:19 -0700

    u

--- Runner/runner.cpp
+++ Runner/runner.cpp
@@ -6,6 +6,7 @@ const ll  MOD = 1000000007;
 #define PI acos(-1)
 #define FOR(a)     for(int t=0;t<a;++t)
 #define print(i,s) cout << "Case #" << i+1 << ": " << s << "\n";
+#define fix(x) cout<<setprecision(x)<<fixed
 #define line() "\n" 
 
 void __print(int x) {cerr << x;}
@@ -22,12 +23,12 @@ void __print(const char *x) {cerr << '\"' << x << '\"';}
 void __print(const string &x) {cerr << '\"' << x << '\"';}
 void __print(bool x) {cerr << (x ? "true" : "false");}
 
-template <typename T>
-T output_vector(vector<T> v) { for(auto val : v) cout << val << " "; cout << line();}
+template <typename T_vector>
+void output_vector(const T_vector &v) { for(auto val : v) cout << val << " "; cout << line();}
 template<typename T, typename V>
 void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
 template<typename T>
-void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
 void _print() {cerr << "]\n";}
 template <typename T, typename... V>
 void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
@@ -39,15 +40,7 @@ void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v..
 #endif
 
 void solve(){
-  int n; cin >> n;
-  string v; cin >> v;
-  vector<int> res(n,1);
-  for(int i=1;i<n;++i){
-      if(v[i] > v[i-1])
-         res[i] = 1+res[i-1];
-  }
-  for(auto val : res) cout << val << " ";
-  cout << line();
+   
 }
 
 int main(){

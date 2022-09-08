2751dc665cd2fe3d70d9720b9dfcabe1c49e12fe
Sashank Shukla <ayaz.shukla@gmail.com>
2022-09-07 20:35:07 -0700

    update

--- Runner/runner.cpp
+++ Runner/runner.cpp
@@ -39,15 +39,53 @@ void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v..
 #define debug(x...)
 #endif
 
+map<pair<int,int>, int> mp;
+int bfs(int x,int y,int n, int m, vector<vector<int>>& grid){
+  mp[{x,y}] = 0;
+  queue<pair<int,int>> q;
+   q.push({x,y});
+   grid[x][y] = 1;
+   while (!q.empty()) {
+   int curr_x = q.front().first, curr_y = q.front().second; q.pop();
+   vector<int> dRow = {1,0}, dCol = {0,1};
+   for(int i=0;i<4;++i){
+       int new_x = curr_x + dRow[i], new_y = curr_y + dCol[i];
+       if(new_x < 0 || new_y < 0 || new_x >= n || new_y >=m || grid[new_x][new_y] == 1)
+         continue;
+       grid[new_x][new_y] = 1;
+       q.push({new_x,new_y});
+       mp[{new_x,new_y}] = mp[{curr_x,curr_y}] + 1;
+   }
+   }
+   // for(auto p : mp){
+   //    cout << p.first.first << " " << p.first.second << "    " << p.second << line(); 
+   // }
+   if(mp.count({n-1,m-1}))
+      return mp[{n-1,m-1}];
+   else
+      return -1;
+}
+
 void solve(){
-   
+    int n,m,x,y,d; cin >> n >> m >> x >>y >> d;
+    vector<vector<int>> grid(n,vector<int> (m,1));
+    --x; --y;
+    for(int i=0;i<n;++i){
+      for(int j=0;j<m;++j){
+          int dist = abs(i-x) + abs(j-y);
+          if(dist > d){
+             grid[i][j] = 0;
+          }
+      }
+    }
+    cout << bfs(0,0,n,m,grid) << line();
+    mp.clear();
 }
 
 int main(){
  ios::sync_with_stdio(0); cin.tie(0);
  int T; cin >> T;
  FOR(T){
-  cout << "Case #" << t+1 << ": ";
   solve();
  }
 }

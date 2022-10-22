45512dd918e389771e937548e0aad86636c8003b
Sashank Shukla <ayaz.shukla@gmail.com>
2022-10-22 00:55:09 -0700

    update

--- Kickstart/2022/Round G/B.cpp
+++ Kickstart/2022/Round G/B.cpp
@@ -39,64 +39,15 @@ void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v..
 #define debug(x...)
 #endif
 
+void solve(){
 
-
-string findMiddleCourse(vector<vector<string>> edges)
-{
-    
-     set<string> left,right;
-     map<string,string> prereqs;
-
-     for(auto edge : edges){
-        left.insert(edge[0]);
-        right.insert(edge[1]);
-        prereqs[edge[0]] = edge[1];
-     }
-
-     vector<string> path; string start;
-     for(auto course : left){
-        if(!right.count(course)){
-            start = course;
-            break;
-        }
-     }
-
-     while(start != "") {
-        path.push_back(start);
-        if(prereqs.count(start)){
-              start = prereqs[start];
-        }
-        else{
-              start = "";
-        }
-     }
-
-     if(path.size() %2 == 0)
-       return path[path.size()/2 -1];
-       return path[path.size()/2];
 }
 
 int main(){
  ios::sync_with_stdio(0); cin.tie(0);
- vector<vector<string>> courses = {
-      {"Foundations of Computer Science", "Operating Systems"},
-      {"Data Structures", "Algorithms"},
-      {"Computer Networks", "Computer Architecture"},
-      {"Algorithms", "Foundations of Computer Science"},
-      {"Computer Architecture", "Data Structures"},
-      {"Software Design", "Computer Networks"}};
-
-    vector<vector<string>> prereqs_courses2 = {
-      {"Data Structures", "Algorithms"},
-      {"Algorithms", "Foundations of Computer Science"},
-      {"Foundations of Computer Science", "Logic"},
-    };
-
-    vector<vector<string>> prereqs_courses3 = {
-      {"Data Structures", "Algorithms"},
-    };
-
-    cout << findMiddleCourse(courses) << line();
-    cout << findMiddleCourse(prereqs_courses2) << line();
-    cout << findMiddleCourse(prereqs_courses3) << line();
-}
+ int T; cin >> T;
+ FOR(T){
+  cout << "Case #" << t+1 << ": ";
+  solve();
+ }
+}           
